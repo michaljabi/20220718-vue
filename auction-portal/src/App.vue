ef15fc3c888e1283e653cae0f134f083fae05465
Jabi <michal.jabi@gmail.com>
2022-07-19 15:08:39 +0200

    feat[auction-portal] (TheHeader): new component.

--- auction-portal/src/App.vue
+++ auction-portal/src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
+
+const myNumber = 2;
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <TheHeader :text="'Auction Portal ' + myNumber"/> 
 </template>
 
 <style scoped>
---
+++ auction-portal/src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    props: { text: String },
+}
+</script>
+
+<template>
+    <header class="p-5 mb-4 bg-light rounded-3">
+        <h1>{{text}}</h1>
+    </header>
+</template>
