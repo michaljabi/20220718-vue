2293c1d8f4e160d4a03b8ebf7315c3d02c2b3dec
Jabi <michal.jabi@gmail.com>
2022-07-20 09:23:04 +0200

    feat[auction-portal] (TheMenu): collapse button works

--- auction-portal/src/App.vue
+++ auction-portal/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
+import TheMenu from './components/TheMenu.vue';
 
 const myNumber = 2;
 </script>
 
 <template>
   <TheHeader :text="'Auction Portal ' + myNumber"/> 
+  <TheMenu />
+
 </template>
 
 <style scoped></style>
---
+++ auction-portal/src/components/TheMenu.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const isMenuShown = ref(true)
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" @click="isMenuShown = !isMenuShown">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" :class="{ show: isMenuShown }">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="auctions">Aukcje</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="promotions">Promocje</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="advices">Podpowiadamy</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
