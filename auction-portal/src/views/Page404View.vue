d2808577149076e819ceaa7e58b581cf3696b028
Jabi <michal.jabi@gmail.com>
2022-07-20 10:58:59 +0200

    feat[auction-portal] (Page404View):  use router and route form code (logic)

--- auction-portal/src/views/Page404View.vue
+++ auction-portal/src/views/Page404View.vue
@@ -1,3 +1,16 @@
+<script setup>
+    import { useRoute, useRouter } from 'vue-router'
+
+    const route = useRoute();
+    const router = useRouter();
+
+    function handleGoHomeClick() {
+        router.push('/')
+    }
+</script>
+
 <template>
-    <h2>Page not found...</h2>
+    <h2>Page <code>/{{route.params.pathMatch.join('/')}}</code> not found...</h2>
+    <hr />
+    <button class="btn btn-primary" @click="handleGoHomeClick">Go back to home</button>
 </template>
