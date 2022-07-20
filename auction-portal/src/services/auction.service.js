8306a76a1917b9902492db01edd5596dd6e398b4
Jabi <michal.jabi@gmail.com>
2022-07-20 13:04:07 +0200

    refactor[auction-portal] (.env): use BACKEND_URL in auction.service.js

--- auction-portal/src/services/auction.service.js
+++ auction-portal/src/services/auction.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000'
+const baseURL = import.meta.env.VITE_BACKEND_URL
 const auctionsURL = `${baseURL}/auctions`
 
 export const auctionService = {
