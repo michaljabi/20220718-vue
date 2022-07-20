e01700cf26abd9884d172aa15031733c5f80a02a
Jabi <michal.jabi@gmail.com>
2022-07-20 10:05:49 +0200

    feat[auction-portal] (router):  step #1 - define a view for routing.

--- auction-portal/src/App.vue
+++ auction-portal/src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
 import TheMenu from './components/TheMenu.vue';
+import AuctionsView from './views/AuctionsView.vue';
 
 const myNumber = 2;
 </script>
 
 <template>
   <TheHeader :text="'Auction Portal ' + myNumber"/> 
-  <TheMenu />
-
+  <main class="container">
+    <TheMenu />
+    <AuctionsView />
+  </main>
 </template>
 
 <style scoped></style>
---
+++ auction-portal/src/views/AuctionsView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h2>Lista naszych aukcji</h2>
+</template>
