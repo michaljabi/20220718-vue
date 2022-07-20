f2c7c074680cf0dbebf89c810f6cb45a622de347
Jabi <michal.jabi@gmail.com>
2022-07-20 10:34:35 +0200

    feat[auction-portal] (router):  step #3 - add rest of the Views.

--- auction-portal/src/router/index.js
+++ auction-portal/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import AuctionsView from '../views/AuctionsView.vue'
 import AuctionsView from '@/views/AuctionsView.vue'
+import PromotionsView from '@/views/PromotionsView.vue'
+import AdvicesView from '@/views/AdvicesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +10,16 @@ const router = createRouter({
     {
       path: '/auctions',
       component: AuctionsView
-    }
+    },
     // Zadanie: dodaj konfigurację do ścieżek promotions i advices
+    {
+      path: '/promotions',
+      component: PromotionsView
+    },
+    {
+      path: '/advices',
+      component: AdvicesView
+    }
   ]
 })
 
---
+++ auction-portal/src/views/AdvicesView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h2>Podpowiadamy co wybrać...</h2>
+</template>
