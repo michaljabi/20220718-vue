779a08100dbd0dce70421344a408fc86df8f5a0c
Jabi <michal.jabi@gmail.com>
2022-07-22 13:23:36 +0200

    refactor[how-to-test-this]: logic of ItemReating refactored after tests.

--- how-to-test-this/src/task-for-day-4/ItemRating.vue
+++ how-to-test-this/src/task-for-day-4/ItemRating.vue
@@ -1,6 +1,9 @@
 <script>
 const MAX_RATE = 5
 const MIN_RATE = 1
+
+const isBetweenExpectedValues = (value) =>
+  value >= MIN_RATE && value <= MAX_RATE
 </script>
 
 <script setup="">
@@ -11,15 +14,15 @@ const props = defineProps({
   startRating: number().def(MAX_RATE),
 })
 
-const isBetweenExpectedValues = props.startRating >= MIN_RATE && props.startRating <= MAX_RATE
-const numberOfStars = isBetweenExpectedValues ? props.startRating : MAX_RATE
+const numberOfStars = isBetweenExpectedValues(props.startRating)
+  ? props.startRating
+  : MAX_RATE
 const currentRating = ref(numberOfStars)
 
 function addRate(amount) {
   const { value } = currentRating
   const rating = value + amount
-  const isBetweenExpectedValues = rating >= MIN_RATE && rating <= MAX_RATE
-  if (isBetweenExpectedValues) {
+  if (isBetweenExpectedValues(rating)) {
     currentRating.value = rating
   }
 }
@@ -28,10 +31,20 @@ function addRate(amount) {
 <template>
   <section>
     <code>({{ currentRating }})</code>
-    <span data-testid="star" v-for="index in currentRating" :key="index">⭐</span>
+    <span data-testid="star" v-for="index in currentRating" :key="index"
+      >⭐</span
+    >
     <div class="p-2">
-      <button data-testid="dec" class="btn btn-secondary" @click="addRate(-1)">-</button>
-      <button data-testid="inc" class="btn btn-primary mx-2" @click="addRate(1)">+</button>
+      <button data-testid="dec" class="btn btn-secondary" @click="addRate(-1)">
+        -
+      </button>
+      <button
+        data-testid="inc"
+        class="btn btn-primary mx-2"
+        @click="addRate(1)"
+      >
+        +
+      </button>
     </div>
   </section>
 </template>
